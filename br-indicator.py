d88a243607501acc2e3dfcf5499830f1c4f0ebb8
fernando <ferherranz@gmail.com>
2015-12-08 21:26:47 +0100

    Implements br_value function that sets the screen brightness

--- br-indicator.py
+++ br-indicator.py
@@ -5,34 +5,43 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 import appindicator
+import os
+
+ITEMS_NUM = 11
 
 class BrightnessIndicator:
     def __init__(self):
         self.ind = appindicator.Indicator ("brightness-indicator", "indicator-messages", appindicator.CATEGORY_APPLICATION_STATUS)
         self.ind.set_status (appindicator.STATUS_ACTIVE)
         self.ind.set_attention_icon ("indicator-messages-new")
-        self.ind.set_icon("distributor-logo")
+        self.ind.set_icon("weather-clear")
 
         # create a menu
         self.menu = gtk.Menu()
-
-        radio = gtk.RadioMenuItem(None, "Radio Menu Item")
-	radio.connect("activate",self.setbr, 20)
-        radio.show()
-        self.menu.append(radio)
+        self.items = []
+        radio_group = gtk.RadioMenuItem()
+        for i in range(ITEMS_NUM):
+            radio = gtk.RadioMenuItem(radio_group, str(i))
+            radio.connect("activate",self.setbr, i*10)
+            radio.show()
+            self.items.append(radio)
+            self.menu.append(radio)
 
         image = gtk.ImageMenuItem(gtk.STOCK_QUIT)
         image.connect("activate", self.quit)
         image.show()
         self.menu.append(image)
-                    
+
         self.menu.show()
+
 
         self.ind.set_menu(self.menu)
 
-    
+
     def setbr(self, widget, value):
-        print value
+        command="xbacklight =" + str(value)
+        os.popen(command)
+
 
     def quit(self, widget, data=None):
         gtk.main_quit()
